8a65ea89626e562571c481a51eb8567aedb5b25c
CVT17 <158773525+CVT17@users.noreply.github.com>
2026-02-24 00:19:25 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-console.log(2+2)
-console.log("Привет мир")
+let ege = 4
+console.log(ege)
+
+let a = "Добро "
+let b = "Пожаловать "
+let c = "На курс "
+let name = "Сергей"
+console.log(a+b+c+name)
+
+alert ("17 02 2026")
+//alert("Привет мир")
